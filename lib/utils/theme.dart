91e119840fdd9f8abf9f8a1bc1821aa6258b82e0
Marc <marcsans26@gmail.com>
2024-02-09 13:50:50 +0100

    millorar els temes

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MyTheme {
   static ThemeData get lightTheme {
     return ThemeData(
+        // definicion del estilo del AppBar
         appBarTheme: const AppBarTheme(
           color: MyColors.easternBlue, // Color del AppBar
           titleTextStyle: TextStyle(
@@ -11,6 +12,8 @@ class MyTheme {
           ),
           actionsIconTheme: IconThemeData(color: MyColors.greenVogue),
         ),
+
+        // definicion del estilo de la pantalla
         scaffoldBackgroundColor: MyColors.cornflower,
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
@@ -18,16 +21,53 @@ class MyTheme {
             color: MyColors.greenVogue, // Color del texto
           ), // Color de fondo de pantalla
         ),
+
         //definicion del estilo de los floating action buttons
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           backgroundColor: MyColors.greenVogue,
           foregroundColor: MyColors.cornflower,
         ),
+
+        // definicion del estilo de los campos de texto
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: TextStyle(color: MyColors.selectiveYellow),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: MyColors.easternBlue,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: MyColors.selectiveYellow,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          filled: true,
+          fillColor: MyColors.easternBlue,
+        ),
+
+        // definicion del estilo de los botones
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size.fromWidth(double.maxFinite),
+            backgroundColor: MyColors.selectiveYellow,
+            foregroundColor: MyColors.greenVogue,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 16,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
+        ),
       );
   }
 
   static ThemeData get darkTheme {
-
     return ThemeData(
         appBarTheme: const AppBarTheme(
           color: MyColors.midnight, // Color del AppBar
@@ -37,6 +77,7 @@ class MyTheme {
           ),
           actionsIconTheme: IconThemeData(color: MyColors.amber),
         ),
+        
         scaffoldBackgroundColor: MyColors.midnightBlue,
         textTheme: const TextTheme(
           bodyMedium: TextStyle(
@@ -44,10 +85,44 @@ class MyTheme {
             color: MyColors.amber, // Color del texto
           ), // Color de fondo de pantalla
         ),
+        
         //definicion del estilo de los floating action buttons
         floatingActionButtonTheme: const FloatingActionButtonThemeData(
           backgroundColor: MyColors.amber,
           foregroundColor: MyColors.midnight,
+        ),
+
+        // definicion del estilo de los campos de texto
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: TextStyle(color: MyColors.gold),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: MyColors.gold,
+              width: 2,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(16)),
+          ),
+          filled: true,
+          fillColor: MyColors.midnight,
+        ),
+     
+        // definicion del estilo de los botones
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size.fromWidth(double.maxFinite),
+            backgroundColor: MyColors.gold,
+            foregroundColor: MyColors.midnightBlue,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 16,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+          ),
         ),
       );
   }
